fbe80eb64f9255c39417a515c025f9a0b99b2cb4
raissa borba <raissavborba@gmail.com>
2021-06-21 15:50:41 +0200

    starting project

---
+++ main.js
@@ -0,0 +1,24 @@
+const character = document.getElementById('character');
+const block = document.getElementById('block');
+const game = document.getElementById('game');
+
+window.addEventListener('keydown', jump);
+
+function jump() {
+    if(character.classList != 'animate'){
+        character.classList.add('animate')
+    }
+    setTimeout(() => {
+        character.classList.remove('animate')
+    }, 500);
+};
+
+const checkDead = setInterval(() => {
+    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
+    if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        alert('You lost!');
+    }
+}, 10);
